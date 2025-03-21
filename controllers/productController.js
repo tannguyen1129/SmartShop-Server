82ae51559b3db3f6d7df92fc198ff1da1e8cfd45
Son Tan <tandtnt15@gmail.com>
2025-03-21 11:56:09 +0700

    Them vai dong vao product.js va productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,37 @@
+const { validationResult } = require('express-validator')
 const models = require('../models')
+
+
 
 exports.getAllProducts = async (req, res) => {
     const product = await models.Product.findAll({})
     res.json(products)
+}
+
+exports.create = async (req, res) => {
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        const msg = error.array().map(error => error.msg).join('')
+        return res.status(422).json({message: msg, success: false})
+    }
+
+    const { name, description, price, photo_url, user_id } = req.body
+
+    try {
+        const newProduct = await models.Product.create({
+            name: name,
+            description: description,
+            price: price,
+            photo_url: photo_url,
+            user_id: user_id 
+        })
+
+        res.status(201).json({success:true, product: newProduct})
+
+    } catch(error) {
+        res.status(500).json({message: "Internal server error", success: false});
+    }
+
 }
--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router() 
 const productController = require('../controllers/productController')
+const { body } = require('express-validator')
+
+const productValidator = [
+    body('name', 'name cannot be empty,').not().isEmpty(),
+    body('description', 'description cannot be empty.').not().isEmpty,
+    body('price', 'price cannot be empty.').not().isEmpty,
+    body('photo_url')
+    .notEmpty().withMessage('photoUrl cannot be empty.')
+]
 
 router.get('/', productController.getAllProducts)
+router.post('/', productValidator, productController.create)
 
 
 module.exports = router
